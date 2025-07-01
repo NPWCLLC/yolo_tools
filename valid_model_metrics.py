ff948cefadde485ecab07087303b14846b90f53c
Denis Gaev <denisgaev@live.ru>
2025-07-01 21:06:42 +0500

    Enhance `valid_model_metrics.py` with logging, device checks, and configuration updates
    Refactored the script to include logging for enhanced traceability, device checks for CUDA availability and GPU details, and updated dataset and model configurations for clearer handling. Replaced print statements with logging and improved code structure for better maintainability.

--- valid_model_metrics.py
+++ valid_model_metrics.py
@@ -1,46 +1,78 @@
 import os
 
+
+
 if __name__ == '__main__':
-    from ultralytics import YOLO
+    from ultralytics import YOLO, checks
+    import torch
+    import torchvision
+    import os
+    from utils import setting_logs
+
+    LOG_FILE = 'valid_model_metrics.log'
+    logging = setting_logs(LOG_FILE)
+
+    os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
+
+    checks()
+
+    DEVICE = []
+    logging.info(f"PyTorch version: {torch.__version__}")
+    logging.info(f"torchvision version: {torchvision.__version__}")
+    logging.info(f"CUDA available: {torch.cuda.is_available()}")
+
+    if torch.cuda.is_available():
+        num_devices = torch.cuda.device_count()
+        logging.info(f"Number of devices: {num_devices}")
+
+        for i in range(num_devices):
+            DEVICE.append(i)
+            logging.info(f"Device {i}: {torch.cuda.get_device_name(i)}")
+    else:
+        logging.info("No CUDA-compatible devices found. Using CPU.")
+
 
     # Загрузка модели
-    model = YOLO("./SSL-PUPS/train_exp/weights/best.pt")
+    model = YOLO("C:\\Users\\omen_\\OneDrive\\Desktop\\PV_data\\PV-SEG-TRAIN\\train_pv-seg_yolo11x_\\weights\\best.pt")
 
     # Оценка модели
-    datasets = os.path.expanduser("~/DENIS_GAEV/TRAIN/TEMP-GAEV/dataset_boxes_75-20-5_ssl-pups_yolo_all/data.yaml")
-    metrics = model.val(data=datasets, imgsz=512, batch=32, device=[0,1],
-        project="SSL-PUPS", name="val_boxes_exp111111",)
+    datasets = os.path.expanduser("F:\\Largha\\SPLIT_PV-SEG\\data.yaml")
+    metrics = model.val(data=datasets, imgsz=640, batch=1, device=DEVICE,
+        project="PV-SEG", name="val_seg",)
+
+    logging.info(f"Model: {model.model_name}")
+    logging.info(f"Dataset: {datasets}")
 
     # Метрики точности (для детекций коробок)
-    print("mAP50-95 (Box Detection): Средняя точность на IoU от 0.5 до 0.95")
-    print("mAP50-95: ", metrics.box.map)
-    print("mAP50 (Box Detection): Средняя точность на IoU = 0.5")
-    print("mAP50: ", metrics.box.map50)
-    print("mAP75 (Box Detection): Средняя точность на IoU = 0.75")
-    print("mAP75: ", metrics.box.map75)
-    print("mAP50-95 per class (Box Detection): ", metrics.box.maps)
+    logging.info("mAP50-95 (Box Detection): Средняя точность на IoU от 0.5 до 0.95")
+    logging.info(f"mAP50-95: {metrics.box.map}")
+    logging.info("mAP50 (Box Detection): Средняя точность на IoU = 0.5")
+    logging.info(f"mAP50: {metrics.box.map50}")
+    logging.info("mAP75 (Box Detection): Средняя точность на IoU = 0.75")
+    logging.info(f"mAP75: {metrics.box.map75}")
+    logging.info(f"mAP50-95 per class (Box Detection): {metrics.box.maps}")
 
     # Метрики полноты и точности (по классам или усреднённые можно взять через mean_results)
     box_results = metrics.box.mean_results()  # Получить усреднённые результаты для детекций
-    print("Mean Box Precision (mp): ", box_results[0])
-    print("Mean Box Recall (mr): ", box_results[1])
+    logging.info(f"Mean Box Precision (mp): {box_results[0]}")
+    logging.info(f"Mean Box Recall (mr): {box_results[1]}")
 
     # Метрики сегментации (для масок)
     if metrics.task != 'detect':
-        print("mAP50-95 (Segmentation): Средняя точность на IoU от 0.5 до 0.95")
-        print("mAP50-95 (Segmentation): ", metrics.seg.map)
-        print("mAP50 (Segmentation): Средняя точность на IoU = 0.5")
-        print("mAP50 (Segmentation): ", metrics.seg.map50)
-        print("mAP75 (Segmentation): Средняя точность на IoU = 0.75")
-        print("mAP75 (Segmentation): ", metrics.seg.map75)
-        print("mAP50-95 per class (Segmentation): ", metrics.seg.maps)
+        logging.info("mAP50-95 (Segmentation): Средняя точность на IoU от 0.5 до 0.95")
+        logging.info(f"mAP50-95 (Segmentation):  {metrics.seg.map}")
+        logging.info("mAP50 (Segmentation): Средняя точность на IoU = 0.5")
+        logging.info(f"mAP50 (Segmentation): {metrics.seg.map50}")
+        logging.info(f"mAP75 (Segmentation): Средняя точность на IoU = 0.75")
+        logging.info(f"mAP75 (Segmentation): {metrics.seg.map75}")
+        logging.info(f"mAP50-95 per class (Segmentation): {metrics.seg.maps}")
 
         seg_results = metrics.seg.mean_results()
-        print("Mean Segmentation Precision (mp): ", seg_results[0])
-        print("Mean Segmentation Recall (mr): ", seg_results[1])
+        logging.info(f"Mean Segmentation Precision (mp): {seg_results[0]}")
+        logging.info(f"Mean Segmentation Recall (mr): {seg_results[1]}")
 
     # Метрики скорости
-    print("Inference time (ms): Время на обработку одного кадра")
-    print("Inference time (ms): ", metrics.speed['inference'])  # Время на обработку
-    print("Frames per second (FPS): Количество кадров, обрабатываемых за секунду")
-    print("FPS: ", 1000 / metrics.speed['inference'])  # FPS на основе времени вывода
+    logging.info("Inference time (ms): Время на обработку одного кадра")
+    logging.info(f"Inference time (ms): {metrics.speed['inference']}")  # Время на обработку
+    logging.info("Frames per second (FPS): Количество кадров, обрабатываемых за секунду")
+    logging.info(f"FPS: {1000 / metrics.speed['inference']}")  # FPS на основе времени вывода
